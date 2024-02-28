8d24c2c70d5cb02f3b526b763af5ea16087fff71
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-02-28 17:47:56 -0400

    feat: add simulation

--- src/fire.ts
+++ src/fire.ts
@@ -3,8 +3,6 @@ interface Config {
     propagation: number;
 }
 
-type CallbackPropagation = (x: number, y: number, propagation: number) => void;
-
 class Fire {
     public data: number[][];
     private config: Config;
@@ -19,17 +17,16 @@ class Fire {
         return this.data.length === 0;
     }
 
-    private addPropagation(
-        row: number,
-        column: number,
-        propagation: number,
-        onPropagate?: CallbackPropagation
-    ) {
-        const { size } = this.config;
+    private addPropagation(row: number, column: number, propagation: number) {
+        const { size, propagation: maxPropagation } = this.config;
         if (column >= 0 && column < size && row >= 0 && row < size) {
-            this.data[row][column] +=
-                this.data[row][column] + propagation >= 0 ? propagation : 0;
-            onPropagate?.(column, row, propagation);
+            const newLevel = this.data[row][column] + propagation;
+            this.data[row][column] =
+                newLevel >= 0
+                    ? newLevel <= maxPropagation
+                        ? newLevel
+                        : maxPropagation
+                    : 0;
         }
     }
 
@@ -43,10 +40,14 @@ class Fire {
     }
 
     putOut() {
+        const chance = 0.2;
         const { size } = this.config;
         for (let row = 0; row < size; row++)
-            for (let column = 0; column < size; column++)
-                this.addPropagation(row, column, -1);
+            for (let column = 0; column < size; column++) {
+                const chanceOfPutOut = Math.random();
+                if (chanceOfPutOut < chance)
+                    this.addPropagation(row, column, -1);
+            }
     }
 
     reset() {
@@ -54,7 +55,7 @@ class Fire {
         this.initData();
     }
 
-    propagate(x: number, y: number, onPropagate?: CallbackPropagation) {
+    propagate(x: number, y: number) {
         const { propagation, size } = this.config;
         let currentPropagation = propagation;
 
@@ -64,39 +65,19 @@ class Fire {
 
             for (let column = x - level; column <= x + level; column++) {
                 let row = y - level;
-                this.addPropagation(
-                    row,
-                    column,
-                    currentPropagation,
-                    onPropagate
-                );
+                this.addPropagation(row, column, currentPropagation);
 
                 if (level !== 0) {
                     row = y + level;
-                    this.addPropagation(
-                        row,
-                        column,
-                        currentPropagation,
-                        onPropagate
-                    );
+                    this.addPropagation(row, column, currentPropagation);
                 }
             }
             for (let row = y - level + 1; row < y + level; row++) {
                 let column = x - level;
-                this.addPropagation(
-                    row,
-                    column,
-                    currentPropagation,
-                    onPropagate
-                );
+                this.addPropagation(row, column, currentPropagation);
                 if (level !== 0) {
                     column = x + level;
-                    this.addPropagation(
-                        row,
-                        column,
-                        currentPropagation,
-                        onPropagate
-                    );
+                    this.addPropagation(row, column, currentPropagation);
                 }
             }
             currentPropagation--;
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,47 @@
-export {};
-console.log('Hello world');
+import { Fire } from './fire';
+
+const app = document.querySelector<HTMLDivElement>('#app');
+
+const GRID_SIZE = [50, 50];
+const FPS = 15;
+const fire = new Fire({
+    size: GRID_SIZE[0],
+    propagation: 10,
+});
+
+function main() {
+    createStructure();
+    setInterval(loop, 1000 / FPS);
+}
+
+function createStructure() {
+    const [width, height] = GRID_SIZE;
+    for (let row = 0; row < height; row++) {
+        const inlineDiv = document.createElement('div');
+        inlineDiv.className = 'inline';
+        for (let column = 0; column < width; column++) {
+            const newDiv = document.createElement('div');
+            newDiv.className = `fire`;
+            newDiv.id = `r${row}c${column}`;
+            newDiv.addEventListener('click', () => {
+                fire.propagate(column, row);
+            });
+            inlineDiv.appendChild(newDiv);
+        }
+        app?.appendChild(inlineDiv);
+    }
+}
+
+function loop() {
+    const [width, height] = GRID_SIZE;
+    for (let row = 0; row < height; row++) {
+        for (let column = 0; column < width; column++) {
+            const currentLevel = fire.data[row][column];
+            const element = document.querySelector(`.fire#r${row}c${column}`);
+            if (element) element.classList.value = `fire l${currentLevel}`;
+        }
+    }
+    fire.putOut();
+}
+
+main();
